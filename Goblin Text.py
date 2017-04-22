41e3da5247bbfe8b57d4eefc1b544826e58f2caa
qmannt <quinnscomputer@Quinns-MacBook-Pro.local>
2017-04-22 15:52:22 -0400

    All continues from intro to loot

--- Goblin Text.py
+++ Goblin Text.py
@@ -1,8 +1,15 @@
 
-answer1 = input("Hello!, This game is called The Adventure, it is about a man who wants to find treasure, your goal is to find it. It's a dangerous world out there, a single wrong choice could kill you. Do you want to play? Y/N")
+answer1 = input("Hello!, This game is called The Adventure, it is about a man who wants to find treasure, your goal is to find it. It's a dangerous world out there, a single wrong choice could kill you. Do you want to play? *note answers are case sensitive* Y/N")
 
-if answer1 == Y:
-    print("Ok, lets start! You start in an armoury, pick a weapon")
-    weapon = input(" ")
+if answer1 == "Y":
+    print("Ok, lets start! You leave your hometown and travel for two days.")
+
+    choice1 = input("Eventually you come to a crossroads, you can take the bridge over a treacherous river or a long road. pick 'bridge' or 'road' ")
+        if choice1 == "bridge"
+            print("You cross the bridge and a goblin jumps out at you, luckily you have your sword and you kill the goblin, but break your sword in the process, you loot the goblin and find a bow.")
+            choice2 = input("You arrive at a new crossroad with another bridge, pick 'bridge' or 'road'")
+                if choice2 == "road"
+                    choice3 = input("As you walk the road, you spot a group of goblins, you shoot them from a distance with your bow. Choose to approach them and loot them or carry on. Choose 'loot' or 'pass'")
+
 else:
-    print("ok")
+    print("Ok, then, bye bye!")
